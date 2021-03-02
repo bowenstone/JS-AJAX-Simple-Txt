c6ecd5ce3f8e8cf1b8d7c033910ab7fe38b3c1dd
Edward Crawford <erac915@icloud.com>
2021-03-02 15:38:50 -0500

    my files

--- ajax.js
+++ ajax.js
@@ -4,7 +4,7 @@ let xhttps = new XMLHttpRequest();
 
 xhttps.onreadystatechange = function () {
     if (this.readyState == 4 && this.state == 200)  {
-        document.get ElementById("shake").innerHTML = this.responseText;
+        document.getElementById("shake").innerHTML = this.responseText;
     }
 }
 
